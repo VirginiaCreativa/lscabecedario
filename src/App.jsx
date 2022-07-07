e5de78c7e2a6fd25b1e681ad9fb793b247c78ea6
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2022-07-07 15:17:09 -0500

    implement layout part 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react';
+import { useState, Fragment } from 'react';
 import Layouts from './layouts/Layout';
 import './App.css';
 
 function App() {
   return (
-    <>
+    <Fragment>
       <Layouts>
         <h1>Hola</h1>
+        <p>
+          Cupidatat cillum velit amet voluptate cillum aliqua duis anim esse
+          sint. Eiusmod est consectetur ea eu irure ipsum enim ea. Eiusmod quis
+          irure nulla est excepteur ea consectetur minim ea nostrud officia
+          exercitation sint irure. Excepteur magna mollit aliqua ipsum laboris
+          voluptate irure. Ad anim culpa ullamco sit dolor anim exercitation
+          dolore sit anim occaecat anim. Est Lorem exercitation duis adipisicing
+          ut nisi exercitation reprehenderit incididunt.
+        </p>
       </Layouts>
-    </>
+    </Fragment>
   );
 }
 

18587d87fdaa05f0ee3d7d89798591ebaec0c65a
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2022-07-23 12:09:58 -0500

    implement OneLetter part 12 Build

--- dist/assets/AllLetter.bcbbb289.js
+++
@@ -1,1 +0,0 @@
-import{a as e}from"./index.b72f36cf.js";import"./OneLetterStyled.80ce47f2.js";function a(){return e("div",{children:e("h1",{children:"Teclado Letas m\xE1s"})})}export{a as default};
--- dist/assets/GameLetter.9ddd9d3e.js
+++
@@ -1,1 +0,0 @@
-import{a as e}from"./index.b72f36cf.js";function t(){return e("div",{children:e("h1",{children:"Juego Abecedario"})})}export{t as default};
